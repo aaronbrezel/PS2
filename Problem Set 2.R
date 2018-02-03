72fedd02de52c3625165da051e9e7d2b904b2753
aaronbrezel <35546183+aaronbrezel@users.noreply.github.com>
2018-02-03 17:26:01 -0600

    Completed question 1
    After completing the Leemis equation, I also created an r formula for Cho Gain's d. I then created a logical structure so that a user can select which equation they'd like their data to be run through.

--- Problem Set 2.R
+++ Problem Set 2.R
@@ -1,12 +1,31 @@
 #1. 
 
+
 voteTotals <- c(1245,925,1622,2101,42) #test data
 
-leemis <- function(x){ 
+benford <- function(x, leemis, choGains){ 
+  #The following two variables will both be used as such for both Leemis and Cho Gain. They only need to be calculated once and can be used no matter which 'if' statement the function enters 
   sigDigitTable <- table(as.numeric(substr(x,1,1))) #takes the first number in each element and counts how many times each unique digit appears
   numTotals <- length(x) #counts how many elements in the vector. This is necessary for the lemis equation
-  m <- max((sigDigitTable/numTotals) - log10(1 + 1/as.numeric(labels(sigDigitTable)[[1]]))) #crux of the lemis equation, follows the formula.  
-  return(m)
+  if(identical(leemis,TRUE) & identical(choGains,TRUE)){ #If the user inputs 'TRUE' for both Leemis and Cho Gains 
+    m <- max((sigDigitTable/numTotals) - log10(1 + 1/as.numeric(labels(sigDigitTable)[[1]]))) #crux of the Leemis equation, follows the formula.  
+    d <- sqrt(sum(((sigDigitTable/numTotals) - log10(1 + 1/as.numeric(labels(sigDigitTable)[[1]])))^2)) #crux of the leemis equation 
+    return(list("Lemmis' m statistic" = m, "Cho Gain's d" = d)) #Returns both values as a properly labeled list 
+  }
+  if(identical(leemis,TRUE)){ #If the user only sets Leemis as true
+    m <- max((sigDigitTable/numTotals) - log10(1 + 1/as.numeric(labels(sigDigitTable)[[1]])))   #leemis function again
+    return(list("Leemis' m statistic" = m)) #returns as a properly labeled list
+  }
+  if(identical(choGains, TRUE)){ #if the user only sets Cho Gain as tue
+    d <- sqrt(sum(((sigDigitTable/numTotals) - log10(1 + 1/as.numeric(labels(sigDigitTable)[[1]])))^2)) #Cho Gain again
+    return(list("Cho Gain's d" = d)) #returns as a properly labeled list
+  }
+  else{
+    return("You must pick either/or/both Leemis and Cho Gain to be TRUE")
+  }
 }
 
-leemis(voteTotals) #Test of function. It works!
+benford(voteTotals,FALSE,FALSE) #Test of function. It works!
+
+
+
